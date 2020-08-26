aa6c5477ac46d0db39f6d68e1fad6f3ac2480f72
Ansh Shukla <shuklaansh96@gmail.com>
2020-08-26 12:11:22 -0700

    init

---
+++ fan-controller.py
@@ -0,0 +1,20 @@
+"""
+Script to automatically turn the fan on the RPi on/off
+based on CPU temperature thresholds
+"""
+from time import sleep
+from gpiozero import OutputDevice, CPUTemperature
+
+cpu_thermostat = CPUTemperature()
+fan = OutputDevice(14, initial_value=False)
+
+max_threshold = 50
+min_threshold = 35
+
+while True:
+    temp = cpu_thermostat.temperature
+    if temp >= max_threshold:
+        fan.on()
+    elif temp < min_threshold:
+        fan.off()
+    sleep(1)
